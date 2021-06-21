0e9b06c99df32add647d0c8d26951376e25703c3
Diego <lima.j.diego@gmail.com>
2021-06-21 16:45:04 -0300

    Aplicando a injeção de dependência

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -1,14 +1,25 @@
-
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Services;
 
 namespace Tarefas.Controllers
 {
     public class TasksController : Controller
     {
+        ITaskItemService _taskService;
+
+        //Aplicando a injeção de dependência
+        public TasksController(ITaskItemService taskService)
+        {
+            _taskService = taskService;
+            
+        }
+
+
         //método actions index para retorna lista de tarefas
         public IActionResult Index()
-        {
-            return View();
+        {   
+            var tasks = _taskService.GetItemAsync();            
+            return View(tasks);
         }
     }
 }
