27cf86f2c43e003d7c244d3b1ea5f5eff3969136
Diego <lima.j.diego@gmail.com>
2021-06-21 11:49:02 -0300

    Criado método action index

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -5,6 +5,10 @@ namespace Tarefas.Controllers
 {
     public class TasksController : Controller
     {
-        
+        //método actions index para retorna lista de tarefas
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
