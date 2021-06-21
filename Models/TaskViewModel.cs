439c1afcc505c40e3012518b0cf5c456881a62bf
Diego <lima.j.diego@gmail.com>
2021-06-21 15:12:52 -0300

    Criada a classe view model das tarefas

---
+++ Models/TaskViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Tarefas.Models
+{
+    public class TaskViewModel
+    {
+        // Nessa classe vamos exibir as informações que queremos na view(A nossa coleção de tarefas).
+
+        public IEnumerable<TaskItem> TaskItens { get; set;}
+    }
+}
