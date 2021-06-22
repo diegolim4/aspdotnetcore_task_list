72c2a882f5e0023820ae0745088411f2aff8a63a
Diego <lima.j.diego@gmail.com>
2021-06-22 10:23:33 -0300

    Exibindo página de tarefas

--- Models/TaskViewModel.cs
+++ Models/TaskViewModel.cs
@@ -7,5 +7,6 @@ namespace Tarefas.Models
         // Nessa classe vamos exibir as informações que queremos na view(A nossa coleção de tarefas).
 
         public IEnumerable<TaskItem> TaskItens { get; set;}
+        public IEnumerable<TaskItem> TasksItens { get; set; }
     }
 }
