a73576ceebaa86c603f3e016585caba4a3bc468c
Diego <lima.j.diego@gmail.com>
2021-06-24 07:42:02 -0300

    Aplicando data annotations no módelo de domínio

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,15 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tarefas.Models
 {
     public class TaskItem
     {
-        public System.Guid Id { get; set; }
-
+        [Key]
+        public int Id { get; set; }
+        [Display(Name="Tarefa Completa")]
         public bool IsComplete { get; set; }
-    
+        [Required(ErrorMessage ="Nome da tarefa é Obrigatório")]
+        [StringLength(200)]
         public string Name  { get; set; }
-    
+        [Display(Name="Data de Conclusão")]
         public DateTimeOffset? Dataconclusion { get; set; }
     }
 }
